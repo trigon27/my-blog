c353972767fc5fb68ef14ab96fb1ccd91de32fee
trigon27 <ashrafshaikh0778866@gmail.com>
2023-12-09 13:36:53 +0530

    login backend logic is added

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const Users=require('./models/Users')
+const Users=require('./models/Users');
+const jwt=require('jsonwebtoken');
 const  mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const port = 4000;
+const secret='lsjfahsuihejfhh3uffvh';
 
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 mongoose.connect('mongodb+srv://shaikhpc0786:spiderman@cluster0.gkdbxsk.mongodb.net/?retryWrites=true&w=majority')
 app.post('/register', async(req, res) => {
     const {userName,password}=req.body;
-      console.log(userName,password);
+      // console.log(userName,password);
    
   try {
       // Check if username is provided
@@ -28,6 +30,26 @@ app.post('/register', async(req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+app.post('/login',async(req,res)=>{
+  const {userName,password}=req.body;
+  const docData=await Users.findOne({userName});
+ const passOK= bcrypt.compareSync(password,docData.password)
+  // res.json(passOK)
+  if(passOK){
+    //log in
+    jwt.sign({userName,id:docData._id},secret,{},(err,token)=>{
+      if(err) throw err;
+      else{
+        res.cookie('token',token).json('ok');
+      }
+    })
+   
+  }
+  else{
+    res.status(400).json(" wrong crentiations")
+  }
+})
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function login(ev) {
+    ev.preventDefault();
+
+    const response = await fetch('http://localhost:4000/login', {
+      method: 'POST',
+      body: JSON.stringify({ userName, password }),
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+    });
+
+    if (response.ok) {
+      // Redirect to home page
+      navigate('/');
+    } else {
+      alert('Wrong credentials');
+    }
+  }
+
   return (
     <div>
-        <form className='login'>
-            <h1>Log in </h1>
-            <input type='text' placeholder='user name '/>
-            <input type='text' placeholder='password'/>
-            <button>log in</button>
-        </form>
+      <form className="login" onSubmit={login}>
+        <h1>Log in</h1>
+        <input
+          type="text"
+          placeholder="user name"
+          value={userName}
+          onChange={(ev) => setUserName(ev.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onChange={(ev) => setPassword(ev.target.value)}
+        />
+        <button>log in</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
